2a88c2c80df3e7dd08a6c8416574b33fd5c660e0
pohe <pohe@easj.dk>
2017-04-23 16:43:19 +0200

    Second Class created

---
+++ HelloGit/SeconsClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class SeconsClass
+    {
+        public int No { get; set; }
+
+        public SeconsClass(int no)
+        {
+            No = no;
+        }
+    }
+}
