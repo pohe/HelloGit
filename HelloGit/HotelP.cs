6b5cad7272cafff43930a146d6f4bb002623dcde
pohe <pohe@easj.dk>
2017-04-23 16:05:49 +0200

    Correction 1

---
+++ HelloGit/HotelP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class HotelP
+    {
+        public int HotelNo { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+
+        public HotelP(int hotelNo, string name, string adress)
+        {
+            HotelNo = hotelNo;
+            Name = name;
+            Adress = adress;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
